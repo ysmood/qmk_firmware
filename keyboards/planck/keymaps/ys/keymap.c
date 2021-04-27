cc9ab9d122ddb0af0cb29fd451d1b1c3892d9354
Yad Smood <ys@ysmood.org>
2021-04-28 01:58:45 +0900

    update keymap

--- keyboards/planck/keymaps/ys/keymap.c
+++ keyboards/planck/keymaps/ys/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,      
     LCTL_T(KC_TAB), KC_A,           KC_S,           KC_D,           LT(4,KC_F),     KC_G,           KC_H,           LT(5,KC_J),     KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
-    LSFT_T(KC_GRAVE),SCMD_T(KC_Z),   LSA_T(KC_X),   ACMD_T(KC_C),   C_S_T(KC_V),LSFT_T(KC_B),   RSFT_T(KC_N),   KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,       
+    LSFT_T(KC_GRAVE),ACMD_T(KC_Z),           LSA_T(KC_X),    SCMD_T(KC_C),   C_S_T(KC_V),    LSFT_T(KC_B),   RSFT_T(KC_N),   KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,       
     TO(6),          LCTL_T(KC_MINUS),LALT_T(KC_EQUAL),KC_LGUI,        LT(1,KC_LBRACKET),KC_SPACE,       KC_NO,          LT(2,KC_RBRACKET),KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT
   ),
 
